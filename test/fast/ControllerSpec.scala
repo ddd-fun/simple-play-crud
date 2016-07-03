bd2ce6212a95235acb676d9ec1f1c631f02badc0
ddd-fun <laserdev@gmail.com>
2016-07-03 15:21:24 +0200

    code formatting

--- test/fast/ControllerSpec.scala
+++ test/fast/ControllerSpec.scala
@@ -10,12 +10,12 @@ object DomainDataGen{
 
   val validTitleGen = for {
     length <- Gen.choose(2 ,32)
-    str <- Gen.listOfN(length, Gen.alphaChar).map(_.mkString)
+    str    <- Gen.listOfN(length, Gen.alphaChar).map(_.mkString)
   } yield str
 
   val inValidTitleGen = for {
     length <- Gen.oneOf(Gen.choose(0 ,1), Gen.choose(33, 128))
-    str <- Gen.listOfN(length, Gen.alphaChar).map(_.mkString)
+    str    <- Gen.listOfN(length, Gen.alphaChar).map(_.mkString)
   } yield str
 
   val validPriceGen = Gen.choose(0 , 5000000)
@@ -41,40 +41,39 @@ object ControllerValidationSpec extends Properties("Controller validations") {
   def anyExcept(exclude: String) = Gen.alphaStr suchThat(str => str.length > 0 && str != exclude)
 
   val jsonWithInvalidFieldNamesGen = for{
-    guid <- anyExcept("guid")
+    guid  <- anyExcept("guid")
     title <- anyExcept("title")
     price <- anyExcept("price")
-    fuel <- anyExcept("fuel")
+    fuel  <- anyExcept("fuel")
   }yield (Json.obj(guid -> "guid", title -> "title", fuel -> "fuel", "price" -> 1))
 
 
   val validJsonGen = for{
-    guid <- Gen.uuid.map(_.toString)
+    guid  <- Gen.uuid.map(_.toString)
     title <- validTitleGen
-    fuel <- Gen.oneOf("diesel", "gasoline")
-    price <- Gen.choose(0, 5000000)
+    fuel  <- validFuelGen
+    price <- validPriceGen
     usage <- Gen.option(validUsageJsonGen)
   }yield {val json = Json.obj("guid" -> guid, "title" -> title, "fuel" -> fuel, "price" -> price)
           usage.map(json++).getOrElse(json) }
 
 
   val invalidJsonGen = (for{
-    (guid, g) <- Gen.uuid.map(id => (id.toString, 1))
-    (title, t) <- Gen.oneOf(validTitleGen.map((_,1)), inValidTitleGen.map((_,0)))
-    (fuel, f) <- Gen.oneOf(validFuelGen.map((_,1)), invalidFuelGen.map((_,0)))
-    (price, p) <- Gen.oneOf(validPriceGen.map((_,1)), invalidPriceGen.map((_,0)) )
+    (guid,g)  <- Gen.uuid.map(id => (id.toString, 1))
+    (title,t) <- Gen.oneOf(validTitleGen.map((_,1)), inValidTitleGen.map((_,0)))
+    (fuel,f)  <- Gen.oneOf(validFuelGen.map((_,1)), invalidFuelGen.map((_,0)))
+    (price,p) <- Gen.oneOf(validPriceGen.map((_,1)), invalidPriceGen.map((_,0)) )
   }yield ((Json.obj("guid" -> guid, "title" -> title, "fuel" -> fuel, "price" -> price),
            g*t*f*p))) suchThat( _._2 != 1) map (_._1)
 
    val validUsageJsonGen: Gen[JsObject] = for{
-    m <- validMileageGen
+    m   <- validMileageGen
     reg <- validFirstRegGen
   }yield (Json.obj("usage" -> Json.obj("mileage"->m, "firstReg" -> reg)))
 
   
   def genFakeRequest(json:JsObject) = new FakeRequest(
-    method = "POST",
-    uri = "/adverts",
+    method = "POST", uri = "/adverts",
     headers = FakeHeaders(Seq("Content-type"->"application/json")),
     body =  json
   ){override def toString = s"$method $uri body: $body"}
@@ -83,19 +82,24 @@ object ControllerValidationSpec extends Properties("Controller validations") {
   property("return 400 if any of json fields has invalid name") =
     forAll(jsonWithInvalidFieldNamesGen.map(genFakeRequest)) { req =>
     val response = TestApplicationController.addAdvert.apply(req)
-    status(response).equals(BAD_REQUEST) &&  (Json.parse(contentAsString(response) ) \ "status").as[String] == "KO"
+
+    status(response).equals(BAD_REQUEST) &&
+      (Json.parse(contentAsString(response) ) \ "status").as[String] == "KO"
   }
 
   property("return 400 if some json field contains invalid value") =
   forAll(invalidJsonGen.map(genFakeRequest)) { req =>
     val response = TestApplicationController.addAdvert.apply(req)
-    status(response).equals(BAD_REQUEST)  && (Json.parse(contentAsString(response) ) \ "status").as[String] == "KO"
+
+    status(response).equals(BAD_REQUEST)  &&
+      (Json.parse(contentAsString(response) ) \ "status").as[String] == "KO"
   }
 
   property("return 200 for valid request") =
     forAll(validJsonGen.map(genFakeRequest)) { req =>
-      val response = TestApplicationController.addAdvert.apply(req)
-      status(response).equals(OK)
+    val response = TestApplicationController.addAdvert.apply(req)
+
+    status(response).equals(OK)
   }
 
 }
