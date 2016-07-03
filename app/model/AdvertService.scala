fb2d8d3886437e1136e79a94bdfa840435ab3d29
ddd-fun <laserdev@gmail.com>
2016-07-03 18:02:13 +0200

    defined domain algebra

---
+++ app/model/AdvertService.scala
@@ -0,0 +1,32 @@
+package model
+
+
+trait AdvertService[Advert, Id] extends Repository[Advert, Id] {
+
+  def store(id:Id, advert: Advert): Option[Advert] = for {
+    _ <- invert(get(id))
+    s <- saveOrUpdate(advert)
+  }yield s
+
+  def update(id:Id, advert: Advert) : Option[Advert] = for{
+    _ <- get(id)
+    u <- saveOrUpdate(advert)
+  }yield u
+
+  def delete(id: Id) : Option[Advert] = for{
+    a <- get(id)
+    d <- remove(a)
+  }yield d
+
+  def invert[A](o:Option[A]): Option[A]
+}
+
+trait Repository[Advert, Id] {
+
+  def saveOrUpdate(advert: Advert) : Option[Advert]
+
+  def remove(advert: Advert) : Option[Advert]
+
+  def get(id:Id) : Option[Advert]
+
+}
