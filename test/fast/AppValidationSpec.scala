387749600d09569e32611bd14d9f5186403c6768
volodymyr.lazoryshyn <volodymyr.lazoryshyn@wirecard.com>
2016-07-07 15:28:15 +0200

    added labels for validation properties

--- test/fast/AppValidationSpec.scala
+++ test/fast/AppValidationSpec.scala
@@ -3,7 +3,7 @@ package fast
 import fast.DetailedAppSpec._
 import model.ServiceInterpreter
 import org.scalacheck._
-import org.scalacheck.Prop.forAll
+import org.scalacheck.Prop.{forAll, BooleanOperators}
 import play.api.libs.json.{JsObject, Json}
 import play.api.test.{FakeHeaders, FakeRequest}
 
@@ -11,36 +11,37 @@ import play.api.test.{FakeHeaders, FakeRequest}
 object AppValidationSpec extends Properties("Application") with DomainDataGen {
 
   object TestApplicationController extends controllers.Application(ServiceInterpreter)
-   
-  
-  def fakePostAdvertRequest(json:JsObject) = new FakeRequest(
-    method = "POST", uri = "/adverts",
-    headers = FakeHeaders(Seq("Content-type"->"application/json")),
-    body =  json
-  ){override def toString = s"$method $uri body: $body"}
-
 
   property("return 400 if any of json fields has invalid name") =
-    forAll(jsonWithInvalidFieldNamesGen.map(fakePostAdvertRequest)) { req =>
-    val response = TestApplicationController.addAdvert.apply(req)
-
-    status(response).equals(BAD_REQUEST) &&
-      (Json.parse(contentAsString(response) ) \ "status").as[String] == "KO"
+  forAll(jsonWithInvalidFieldNamesGen.map(fakePostAdvertRequest)) { req =>
+    examineBadRequestProperty(req)
   }
 
   property("return 400 if some json field contains invalid value") =
   forAll(invalidJsonGen.map(fakePostAdvertRequest)) { req =>
-    val response = TestApplicationController.addAdvert.apply(req)
-
-    status(response).equals(BAD_REQUEST)  &&
-      (Json.parse(contentAsString(response) ) \ "status").as[String] == "KO"
+    examineBadRequestProperty(req)
   }
 
   property("return 200 for valid request") =
-    forAll(validJsonGen.map(fakePostAdvertRequest)) { req =>
+  forAll(validJsonGen.map(fakePostAdvertRequest)) { req =>
     val response = TestApplicationController.addAdvert.apply(req)
+    val responseStatus = status(response)
+    responseStatus.equals(OK) :| (s"expected http status 200, but actually: $responseStatus")
+  }
 
-    status(response).equals(OK)
+  def fakePostAdvertRequest(json:JsObject) = new FakeRequest(
+      method = "POST", uri = "/adverts",
+      headers = FakeHeaders(Seq("Content-type"->"application/json")),
+      body =  json
+  ){override def toString = s"$method $uri body: $body"}
+
+
+  def examineBadRequestProperty(fakeRequest: FakeRequest[JsObject]) = {
+      val response = TestApplicationController.addAdvert.apply(fakeRequest)
+      val stat = status(response)
+      val content = contentAsString(response)
+    (stat.equals(BAD_REQUEST)) :| (s"expected http status 400, but actually: $stat") &&
+      ((Json.parse(content) \ "status").as[String] == "KO") :| (s"expected json filed status=KO, but actually: $content")
   }
 
 }
